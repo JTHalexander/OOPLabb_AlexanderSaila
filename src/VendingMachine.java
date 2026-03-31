3b1a9759947a08d35cf997248582b2c835848dec
Alexander Saila <saal24aj@student.ju.se>
2026-03-31 11:01:54 +0200

    add private variables, constructor and addBeverage method

--- src/VendingMachine.java
+++ src/VendingMachine.java
@@ -1,11 +1,31 @@
 public class VendingMachine {
 
-    public VendingMachine(){
+    private int capacity;
+    private int emptySpot;
+    private Beverage[] inv;
 
+    public VendingMachine(){
+        this.capacity = 1;
+        this.emptySpot  = 0;
+        this.inv = new Beverage[capacity];
     }
 
     public void showInventory(){
 
+    }
+
+    public void addBeverage(Beverage bev){
+
+        if(emptySpot == capacity){
+            Beverage[] expandedInv = new Beverage[capacity*2];
+            for(int i=0; i<capacity; i++){
+                expandedInv[i] = this.inv[i];
+            }
+            this.inv = expandedInv;
+            this.capacity *= 2;
+        }
+
+        inv[emptySpot++] = bev;
     }
 
 }
