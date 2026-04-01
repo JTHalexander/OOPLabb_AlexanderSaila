a58a8a97cdc4714ff9ce5da963ddba20c86845c7
Alexander Saila <saal24aj@student.ju.se>
2026-04-01 08:55:25 +0200

    Create an array and run it through a vendingmachine object

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,15 @@
 void main() {
 
+    Beverage[] myBevs = {
+            new Beverage("Coca-Cola", 11.50, 10),
+            new Beverage("Fanta", 9.50, 4),
+            new Beverage("Coffee", 5.00, 20),
+            new Beverage("Sprite", 9.50, 6)
+    };
+
+    VendingMachine myVend = new VendingMachine(myBevs);
+    
+    myVend.run();
+    myVend.stats();
+
 }
